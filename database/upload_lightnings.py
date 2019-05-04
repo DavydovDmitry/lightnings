270319b77a463391f39a0c3019db41fd188b2ee6
davydovdmitry <davydofdmitry@gmail.com>
2019-05-04 19:48:35 +0300

    insert lightnings

---
+++ database/upload_lightnings.py
@@ -0,0 +1,31 @@
+import json
+import datetime
+import os
+
+import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .sqlalchemy_declarative import Lightning
+
+
+def upload_lightnings():
+    response = requests.get('http://www.lightnings.ru/vr44_24.php')
+    content = response.content.replace(b'rs', b'"rs"')
+    with open('./output/' + datetime.datetime.now().strftime("%Y.%m.%d") + '.json', 'wb') as output:
+        output.write(content)
+
+    Session = sessionmaker()
+    engine = create_engine(os.environ['DATABASE_URI'][1:-1])
+    Session.configure(bind=engine)
+    session = Session()
+
+    for item in json.loads(content)['rs']:
+        time_start = item['DS']
+        time_end = item['DE']
+        latitude = float(item['p1t']) + float(item['p2t']) + float(item['p3t']) + float(item['p4t'])
+        longitude = float(item['p1n']) + float(item['p2n']) + float(item['p3n']) + float(item['p4n'])
+        quantity = int(item['cnt'])
+        lightning = Lightning(latitude=latitude, longitude=longitude, time_start=time_start, time_end=time_end, quantity=quantity)
+        session.add(lightning)
+    session.commit()
--- source.py
+++ source.py
@@ -1,28 +1,5 @@
-import json
-import datetime
-
-import requests
+from database.upload_lightnings import upload_lightnings
 
 
 if __name__ == "__main__":
-    response = requests.get('http://www.lightnings.ru/vr44_24.php')
-    content = response.content.replace(b'rs', b'"rs"')
-    with open('./output/' + datetime.datetime.now().strftime("%Y.%m.%d") + '.json', 'wb') as output:
-        output.write(content)
-
-    max_longitude = -180.0
-    max_latitude = -90.0
-    min_longitude = 180.0
-    min_latitude = 90.0
-    for item in json.loads(content)['rs']:
-        max_t = max(map(float, [item['p1t'], item['p2t'], item['p3t'], item['p4t']]))
-        max_n = max(map(float, [item['p1n'], item['p2n'], item['p3n'], item['p4n']]))
-        if max_t > max_longitude: max_longitude = max_n
-        if max_n > max_latitude: max_latitude = max_t
-
-        min_t = min(map(float, [item['p1t'], item['p2t'], item['p3t'], item['p4t']]))
-        min_n = min(map(float, [item['p1n'], item['p2n'], item['p3n'], item['p4n']]))
-        if min_t < min_longitude: min_longitude = min_n
-        if min_n < min_latitude: min_latitude = min_t
-    print(min_latitude, '< latitude < ', max_latitude)
-    print(min_longitude, '< longitude < ', max_longitude)
+    upload_lightnings()
