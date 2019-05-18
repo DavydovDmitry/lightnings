7e7ca3d607725551cc2a22dd6b96bc7d6241c9bf
davydovdmitry <davydofdmitry@gmail.com>
2019-05-18 19:37:29 +0300

    package existence check

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,8 +2,14 @@
 
 export $(cat ./local.env)
 
-psql \
---dbname=$DB_NAME \
---host=$DB_IP \
---port=$DB_PORT \
---username=$DB_USER < database/dumps/2019-05-17.dump
+if !([ -d "./database/dumps" ])
+then
+     mkdir "database/dumps"   
+     echo "Put your dump to ./database/dumps"
+else
+    psql \
+    --dbname=$DB_NAME \
+    --host=$DB_IP \
+    --port=$DB_PORT \
+    --username=$DB_USER < database/dumps/2019-05-17.dump
+fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-#SCRIPTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#cd $SCRIPTS_PATH
-#cd ../
-
 export $(cat ./local.env)
 DB_URI=postgresql+psycopg2://${DB_USER}:${DB_PASSWORD}@${DB_IP}:${DB_PORT}/${DB_NAME}
 
+# run database
+docker-compose up -d
+
 # upload media
 source venv/bin/activate
+python database/sqlalchemy_declarative.py
 python source.py
 deactivate
 
-# dump current db state
+# dump current database state
+if !([ -d "./database/dumps" ])
+then
+     mkdir "database/dumps"   
+fi
 ./scripts/dump.sh > database/dumps/`date +%Y-%m-%d`.dump
+
+docker-compose stop
